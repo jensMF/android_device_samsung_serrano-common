426d8f2661bd1baf24c8f34689c43d4b6baa943d
jm.fthal <j.freudenthal@campus.tu-berlin.de>
2016-07-07 08:31:43 +0200

    geändert: liblights/Android.mk

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
+##LOCAL_PATH := $(call my-dir)
 # HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
